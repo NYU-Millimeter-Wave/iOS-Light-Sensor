10b13a828be96e7160524375a1b892415b2adbca
Cole Smith <css459@nyu.edu>
2016-06-15 12:23:27 -0400

    added Launch Screen

--- Light Power Meter/FIlterTuningViewController.swift
+++ Light Power Meter/FIlterTuningViewController.swift
@@ -31,20 +31,54 @@ class FilterTuningViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Navigation Bar
+        let navBar = self.navigationController?.navigationBar
+        navBar?.setBackgroundImage(UIImage(), forBarMetrics: .Default)
+        navBar?.shadowImage = UIImage()
+        navBar?.backgroundColor = UIColor.clearColor()
+        navBar?.translucent = true
+        
+        for label in labels {
+            label.layer.cornerRadius = 25
+            label.layer.masksToBounds = true
+        }
+        
         if let sen = ip.thresholdSensitivity {
             sliders[0].value = sen
-            labels[1].text = "\(sen)"
+            labels[1].text = String.localizedStringWithFormat("%.2f", sen)
         }
         if let lume = ip.lumeThreshold {
             sliders[1].value = lume
-            labels[2].text = "\(lume)"
+            labels[2].text = String.localizedStringWithFormat("%.2f", lume)
         }
         if let edge = ip.edgeTolerance {
             sliders[2].value = edge
-            labels[3].text = "\(edge)"
+            labels[3].text = String.localizedStringWithFormat("%.2f", edge)
+        }
+        if let rad = ip.closingPixelRadius {
+            stepper.value = Double(rad)
+            labels[0].text = "\(rad)"
         }
     }
 
+    @IBAction func stepperChanged(sender: AnyObject) {
+        self.ip.closingPixelRadius = UInt(stepper.value)
+    }
+    
+    @IBAction func menuPressed(sender: AnyObject) {
+        self.slideMenuController()?.openLeft()
+    }
+    
     @IBAction func slilderChanged(sender: AnyObject) {
+        switch sender.tag {
+        case 0:
+            self.ip.thresholdSensitivity = sliders[0].value
+        case 1:
+            self.ip.lumeThreshold = sliders[1].value
+        case 2:
+            self.ip.edgeTolerance = sliders[2].value
+        default:
+            print("[ ERR ]")
+        }
     }
 }
